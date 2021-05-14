e3695baf0f6303b714bbe57fc3b805a29403bb9e
Kevin Conway <kevinconway@invisionapp.com>
2021-05-14 13:39:56 -0500

    Add raw file name for writing imports
    Using the Name.Split did not work as expected and we got the full path
    from the proto root to the file. The new value provides access to the
    basic file name used for code generation that can be manipulated into
    import paths.

--- tts/tts.go
+++ tts/tts.go
@@ -21,12 +21,21 @@ type tts struct {
 // automatically attached to the BuildContext associated with the ModuleBase.
 func (t *tts) Name() string { return "tts" }
 
+type templateContext struct {
+	pgs.File
+	ProtoFileName string
+}
+
 func (t *tts) Execute(targets map[string]pgs.File, pkgs map[string]pgs.Package) []pgs.Artifact {
 	for _, f := range targets {
 		fname := f.Name().LowerSnakeCase().String()
 		fname = fname[:len(fname)-6] // strip _proto from the value.
-		t.AddGeneratorTemplateFile(fname+"_twirp_pb.js", template.Must(template.New("js").Parse(jsTemplate)), f)
-		t.AddGeneratorTemplateFile(fname+"_twirp_pb.d.ts", template.Must(template.New("ts").Parse(tsTemplate)), f)
+		c := templateContext{
+			File:          f,
+			ProtoFileName: fname,
+		}
+		t.AddGeneratorTemplateFile(fname+"_twirp_pb.js", template.Must(template.New("js").Parse(jsTemplate)), c)
+		t.AddGeneratorTemplateFile(fname+"_twirp_pb.d.ts", template.Must(template.New("ts").Parse(tsTemplate)), c)
 	}
 	return t.Artifacts()
 }
